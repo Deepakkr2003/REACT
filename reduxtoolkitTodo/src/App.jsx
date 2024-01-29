c5409a230eeac6e1252a994a7c999865a4b22008
Deepak <dk1407610@gmail.com>
2024-01-29 16:42:33 +0530

    first commmit

---
+++ reduxtoolkitTodo/src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
